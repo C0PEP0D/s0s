64d7552980ec87868d0c25f940b833a4978ba46c
Rémi Monthiller <monthir@git.inpt.fr>
2025-06-16 17:21:43 +0200

    feat: genralised solver iinterface

--- include/s0s/runge_kutta_fehlberg.h
+++ include/s0s/runge_kutta_fehlberg.h
@@ -57,51 +57,51 @@ class SolverRungeKuttaFehlberg {
 			x += s * dt;
 		}
 
-		template<typename TypeFunction>
-		static void step(const TypeFunction& f, std::vector<std::vector<double>>& x, const double& t, const double& dt) {
-			// assertion
-			assert(("dt should be greater than 0.0. Did you initialize dt ?", dt > 0.0));
-			// computation
-			std::array<std::vector<std::vector<double>>, 5> k;
-			k[0] = f(x, t);
-			// begin s
-			std::vector<std::vector<double>> s = k[0];
-			for(size_t index = 0; index < k[0].size(); ++index) {
-				TypeView<TypeVector>(s[index].data(), s[index].size()) *= b4[0];
-			}
-			// end s
-			for(size_t i = 1; i < k.size(); ++i) {
-				// begin v 
-				std::vector<std::vector<double>> v = k[0];
-				for(size_t index = 0; index < k[0].size(); ++index) {
-					TypeView<TypeVector>(v[index].data(), v[index].size()) *= a[i - 1][0];
-				}
-				// end v
-				for(size_t j = 1; j < i; ++j) {
-					// begin v
-					for(size_t index = 0; index < k[0].size(); ++index) {
-						TypeView<TypeVector>(v[index].data(), v[index].size()) += TypeView<TypeVector>(k[j][index].data(), k[j][index].size()) * a[i - 1][j];
-					}
-					// end v
-				}
-				// begin v
-				for(size_t index = 0; index < k[0].size(); ++index) {
-					TypeView<TypeVector>(v[index].data(), v[index].size()) = TypeView<TypeVector>(x[index].data(), x[index].size()) + TypeView<TypeVector>(v[index].data(), v[index].size()) * dt;
-				}
-				// end v
-				k[i] = f(v, t + c[i] * dt);
-				// begin s
-				for(size_t index = 0; index < k[0].size(); ++index) {
-					TypeView<TypeVector>(s[index].data(), s[index].size()) += TypeView<TypeVector>(k[i][index].data(), k[i][index].size()) * b4[i];
-				}
-				// end s
-			}
-			// begin x
-			for(size_t index = 0; index < k[0].size(); ++index) {
-				TypeView<TypeVector>(x[index].data(), x[index].size()) += TypeView<TypeVector>(s[index].data(), s[index].size()) * dt;
-			}
-			// end x
-		}
+		// template<typename TypeFunction>
+		// static void step(const TypeFunction& f, std::vector<std::vector<double>>& x, const double& t, const double& dt) {
+		// 	// assertion
+		// 	assert(("dt should be greater than 0.0. Did you initialize dt ?", dt > 0.0));
+		// 	// computation
+		// 	std::array<std::vector<std::vector<double>>, 5> k;
+		// 	k[0] = f(x, t);
+		// 	// begin s
+		// 	std::vector<std::vector<double>> s = k[0];
+		// 	for(size_t index = 0; index < k[0].size(); ++index) {
+		// 		TypeView<TypeVector>(s[index].data(), s[index].size()) *= b4[0];
+		// 	}
+		// 	// end s
+		// 	for(size_t i = 1; i < k.size(); ++i) {
+		// 		// begin v 
+		// 		std::vector<std::vector<double>> v = k[0];
+		// 		for(size_t index = 0; index < k[0].size(); ++index) {
+		// 			TypeView<TypeVector>(v[index].data(), v[index].size()) *= a[i - 1][0];
+		// 		}
+		// 		// end v
+		// 		for(size_t j = 1; j < i; ++j) {
+		// 			// begin v
+		// 			for(size_t index = 0; index < k[0].size(); ++index) {
+		// 				TypeView<TypeVector>(v[index].data(), v[index].size()) += TypeView<TypeVector>(k[j][index].data(), k[j][index].size()) * a[i - 1][j];
+		// 			}
+		// 			// end v
+		// 		}
+		// 		// begin v
+		// 		for(size_t index = 0; index < k[0].size(); ++index) {
+		// 			TypeView<TypeVector>(v[index].data(), v[index].size()) = TypeView<TypeVector>(x[index].data(), x[index].size()) + TypeView<TypeVector>(v[index].data(), v[index].size()) * dt;
+		// 		}
+		// 		// end v
+		// 		k[i] = f(v, t + c[i] * dt);
+		// 		// begin s
+		// 		for(size_t index = 0; index < k[0].size(); ++index) {
+		// 			TypeView<TypeVector>(s[index].data(), s[index].size()) += TypeView<TypeVector>(k[i][index].data(), k[i][index].size()) * b4[i];
+		// 		}
+		// 		// end s
+		// 	}
+		// 	// begin x
+		// 	for(size_t index = 0; index < k[0].size(); ++index) {
+		// 		TypeView<TypeVector>(x[index].data(), x[index].size()) += TypeView<TypeVector>(s[index].data(), s[index].size()) * dt;
+		// 	}
+		// 	// end x
+		// }
 
 		template<typename TypeFunction>
 		static void step(const TypeFunction& f, double** pXArray, const unsigned int* pXSize, const unsigned int xArraySize, const double& t, const double& dt) {
